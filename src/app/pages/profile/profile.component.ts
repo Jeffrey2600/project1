0da9a7391528452d0ead54747c194bf68ee5f5e2
rv4243 <rvasanth4243@gmail.com>
2024-10-02 11:03:51 +0530

    profile about

---
+++ src/app/pages/profile/profile.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-profile',
+  standalone: true,
+  imports: [],
+  templateUrl: './profile.component.html',
+  styleUrl: './profile.component.css'
+})
+export class ProfileComponent {
+
+  customer = {
+    firstName: 'Andrew',
+    lastName: 'Turing',
+    timeZone: '+5 GMT',
+    phone: '555-237-2384',
+    email: 'andrew.turing@cryptographyinc.com',
+    samlUrl: 'https://cryptographyinc.com/login/secure/kQ28nei9w99'
+  };
+
+  editProfile() {
+    // Code to navigate to or open an edit form
+    console.log('Edit Profile clicked');
+
+  }
+}
